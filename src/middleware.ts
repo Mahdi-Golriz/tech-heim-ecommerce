a38baa8fd74e836e525e5016f0e108809cde8f31
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2025-03-08 17:33:50 +0100

    Refactor the middleware to handle wrong locales ro direct to homePage with default locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,29 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+export function middleware(request: NextRequest) {
+  const locale = request.nextUrl.pathname.split("/")[1];
+
+  // If the locale is not one of the allowed locales, redirect to the default locale (en)
+  if (!routing.locales.includes(locale as "en" | "de")) {
+    return NextResponse.redirect(new URL(routing.defaultLocale, request.url));
+  }
+
+  // Otherwise, continue with next-intl middleware
+  const intlMiddleware = createMiddleware(routing);
+  return intlMiddleware(request);
+}
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(de|en)/:path*"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
