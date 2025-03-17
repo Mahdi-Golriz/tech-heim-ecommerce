4c6685c519477acebe588f392b79b9b54314353c
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2025-03-17 12:12:58 +0100

    add useCategories and useCategory custom hooks

---
+++ src/api/categories.ts
@@ -0,0 +1,41 @@
+import { Category } from "@/models/categories-model";
+import fetcher from "@/utils/fetcher";
+
+interface GetCategoriesParams {
+  params?: Record<string, string>;
+}
+
+interface GetCategoryParams {
+  id: number | string;
+}
+
+interface CategoriesResponse {
+  data: Category[];
+  meta: {
+    pagination: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    };
+  };
+}
+
+interface CategoryResponse {
+  data: Category;
+}
+
+export const getCategories = async ({ params }: GetCategoriesParams) => {
+  return fetcher<CategoriesResponse>({
+    path: "/api/categories",
+    method: "GET",
+    params,
+  });
+};
+
+export const getCategory = async ({ id }: GetCategoryParams) => {
+  return fetcher<CategoryResponse>({
+    path: `/api/categories/${id}`,
+    method: "GET",
+  });
+};
