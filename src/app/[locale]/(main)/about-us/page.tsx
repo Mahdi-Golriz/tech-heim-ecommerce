0a9ef6f3ce5450fa3f2106a0d7f934fa47d0e133
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2025-06-04 16:24:29 +0200

    refactor about and contact-us pages

--- src/app/[locale]/(main)/about-us/page.tsx
+++ src/app/[locale]/(main)/about-us/page.tsx
@@ -1,18 +1,8 @@
-import { CustomBreadcrumb } from "@/components";
 import AboutTechHelm from "@/components/about-us/about-us";
-
-const links = [
-  { href: "/", title: "Home" },
-  {
-    href: "",
-    title: "About us",
-  },
-];
 
 const AboutUsPage = () => {
   return (
     <div className="container">
-      <CustomBreadcrumb links={links} />
       <AboutTechHelm />
     </div>
   );
--- src/app/[locale]/(main)/contact-us/page.tsx
+++ src/app/[locale]/(main)/contact-us/page.tsx
@@ -1,18 +1,8 @@
-import { CustomBreadcrumb } from "@/components";
 import ContactUs from "@/components/contact-us/contact-us";
-
-const links = [
-  { href: "/", title: "Home" },
-  {
-    href: "",
-    title: "Contact us",
-  },
-];
 
 const ContactPage = () => {
   return (
     <div className="container">
-      <CustomBreadcrumb links={links} />
       <ContactUs />
     </div>
   );
